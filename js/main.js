1c42fab3da1182d03d904b21774538e51b9958ac
RosaDowning <rosadowning@hotmail.co.uk>
2019-07-31 18:08:15 +0100

    almost finished

--- js/main.js
+++ js/main.js
@@ -1,15 +1,21 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
 
- new WOW().init();
+  new WOW().init();
 
-  $(window).scroll(function() {
+  $(window).bind('resize', set_body_height);
+  set_body_height();
+
+  $('body').css('display', 'none');
+  $('body').fadeIn(2000);
+ 
+  $(window).scroll(function () {
     if ($(this).scrollTop() > 100) {
       $('.back-to-top').fadeIn('slow');
     } else {
       $('.back-to-top').fadeOut('slow');
     }
   });
-  $('.back-to-top').click(function() {
+  $('.back-to-top').click(function () {
     $('html, body').animate({
       scrollTop: 0
     }, 1500, 'easeInOutExpo');
@@ -17,18 +23,20 @@ jQuery(document).ready(function($) {
   });
 });
 
-$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+$(document).on('click', '[data-toggle="lightbox"]', function (event) {
   event.preventDefault();
   $(this).ekkoLightbox({
     alwaysShowClose: true
   });
 });
 
-function set_body_height() { // set body height = window height
-  $('.hero').height($(window).height());
-}
-$(document).ready(function() {
-  $(window).bind('resize', set_body_height);
-  set_body_height();
-});
+function set_body_height() {
 
+  if ($(window).height() > 620) {
+    $('.hero').height(620);
+  } else {
+    $('.hero').height($(window).height());
+  }
+
+  $('#final-image').height($(window).height() / 2);
+}
